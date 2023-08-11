a180a5d990bb41130e7dad1ec29735a5e5d1bef0
sumitvekariya <sumitvekariya7@gmail.com>
2023-08-11 13:09:20 +0530

    Added basic function interface and few variables

---
+++ src/Amino.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+contract Amino {
+    constant dailyCheckinMax = 1000000000000000000000;
+    struct UserData {
+        
+    }
+
+    function dailyCheckin(uint256 amount)  returns () {
+        
+    }
+
+    function dailyStepReward()  returns () {
+        
+    }
+
+    function dailyChallengeReward()  returns () {
+        
+    }
+
+    function dailyLeaderboardReward()  returns () {
+        
+    }
+
+    function dailyCheckInWheel()  returns () {
+        
+    }
+
+    function referralReward()  returns () {
+        
+    }
+
+    function shoppingRewards()  returns () {
+        
+    }
+
+    function grantPromoCoins()  returns () {
+        
+    }
+}
